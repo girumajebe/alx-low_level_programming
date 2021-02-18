b5f07748331d244ee933f8ae52162f590fb688dd
girumajebe <zr.one3@gmail.com>
2021-02-18 15:57:07 +0000

    fixed

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,21 +8,21 @@
  */
 int main(void)
 {
-  int d;
+int d;
 
-  for (d = 0; d < 90; d++)
-    {
-      for ()
-	{
-	  putchar((d / 10) + '0');
-	  putchar((d % 10) + '0');
-	}
-      if (d != 89)
-	{
-	  putchar(',');
-	  putchar(' ');
-	}
-    }
-  putchar('\n');
-  return (0);
+for (d = 0; d < 90; d++)
+{
+for ()
+{
+putchar((d / 10) + '0');
+putchar((d % 10) + '0');
+}
+if (d != 89)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
