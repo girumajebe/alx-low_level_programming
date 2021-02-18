0657bd120d2644e4b5dabf9bc574cc7ed1647301
girumajebe <zr.one3@gmail.com>
2021-02-18 14:48:15 +0000

    to be checked

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * *_strcat - concatenate two strings
+ * @src: string one
+ * @dest: string two, at the end both strings will save there
+ * Return: dest
+ **/
+char *_strcat(char *dest, char *src)
+{
+int i, j, l;
+
+for (i = 0; dest[i] != 00; i++)
+{
+}
+for (j = 0; src[j] != 00; j++)
+{
+}
+for (l = 0; l < j; l++)
+{
+dest[i + l] = src[l];
+}
+return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * *rot13 - encode an string into rot13
+ * @s: string
+ *
+ * Return: string encoded
+ **/
+char *rot13(char *s)
+{
+int i, j;
+char src[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+char encode[] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+
+for (i = 0; s[i] != 00; i++)
+{
+for (j = 0; src[j] != 00; j++)
+{
+if (s[i] == src[j])
+{
+s[i] = encode[j];
+break;
+}
+}
+}
+return (s);
+}
