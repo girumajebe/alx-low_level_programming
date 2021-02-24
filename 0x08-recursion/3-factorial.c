1a0ce23fe0e4cae7d7f07c977755eb4593d5f94e
girumajebe <zr.one3@gmail.com>
2021-02-24 10:58:42 +0000

    to be checked

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+iassed number
+* @n: passed number
+* Return: Factor of n
+**/
+int factorial(int n)
+{
+if (n > 0)
+{
+return (factorial(n - 1) * n);
+}
+else if (n < 0)
+{
+return (-1);
+}
+else
+{
+return (1);
+}
+}
