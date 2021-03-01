92da4aa404797cb5c5b27335d9f2f01f2af998a3
girumajebe <zr.one3@gmail.com>
2021-03-01 05:17:32 +0000

    ans V1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Will print the name of the executable file
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: 0
+ **/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
