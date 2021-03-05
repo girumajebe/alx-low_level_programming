9cf61340ebb4ac22d0f1471039565c157f1eabee
girumajebe <zr.one3@gmail.com>
2021-03-05 14:03:36 +0000

    updated

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,22 +1,46 @@
-#include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * _realloc - that reallocates a memory block using malloc and free
- * @ptr: pointer
- * @old_size : unsigned integer.
- * @new_size : unsigned integer
- * Return: a pointer to the duplicated string.
+ *_realloc - realloc but with malloc
+ * @newP: new pointer created with malloc
+ * @ptr: pointer created with malloc
+ * @new_size: is the new size
+ */
+void copyPasteK(char *newP, void *ptr, unsigned int new_size)
+{
+unsigned int i = 0;
+
+while (i < new_size)
+{
+*((char *)newP + i) = *((char *)ptr + i);
+i++;
+}
+}
+
+/**
+ * _realloc - realloc but with malloc
+ * @ptr: pointer created with malloc
+ * @old_size: used space
+ * @new_size: is the new size
+ * Return: pointer
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+void *newP;
 
+if (ptr == NULL)
+return (malloc(new_size));
 if (new_size == old_size)
-return (NULL);
-if (new_size == 0 || ptr == NULL)
+return (ptr);
+if (new_size == 0 && ptr != NULL)
 {
 free(ptr);
 return (NULL);
 }
+newP = malloc(new_size);
+if (newP == NULL)
+return (NULL);
+copyPasteK(newP, ptr, new_size);
+free(ptr);
+return (newP);
 }
